167cbd5342c12b15653caf8458b6739fa654a936
Ryan Bowman <rymbowman@gmail.com>
2025-02-21 10:12:45 -0500

    Update styling of app title

--- goalgetter-app/src/app/pages/LandingPage.jsx
+++ goalgetter-app/src/app/pages/LandingPage.jsx
@@ -1,9 +1,20 @@
-import { Box, Typography } from "@mui/material";
+import { Box, styled, Typography } from "@mui/material";
+
+const Headline = styled(Typography)({
+  fontSize: 70,
+  marginTop: 50,
+  letterSpacing: 5,
+  fontFamily: "Kaushan Script",
+  cursive,
+  color: "#50C878",
+  display: "flex",
+  alignItems: "flex-end",
+});
 
 const LandingPage = () => {
   return (
     <Box>
-      <Typography variant="h1">GoalGetter</Typography>
+      <Headline variant="h1">GoalGetter</Headline>
       <Typography variant="body1">
         Helping you reach your goals one task at a time
       </Typography>
